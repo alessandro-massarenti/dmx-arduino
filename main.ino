45042035386af1535536351ad18577366cedfaf8
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-04-04 00:26:24 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,16 @@
 #include <DmxSimple.h>
 
-int v;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String commandString = "";
 
+boolean isConnected = false;
+
+String c;
+String v;
 void setup() {
 
   Serial.begin(9600);
-  Serial.setTimeout(10);
 
   digitalWrite(2, HIGH);
   DmxSimple.usePin(4);
@@ -17,23 +22,49 @@ void setup() {
   DmxSimple.write(1, 0);
   DmxSimple.write(3, 134);
   DmxSimple.write(8, 0);
-
-
-
-
 }
 
 
 void loop() {
 
-  if (Serial.available()) {
-    v = Serial.parseInt();
-    Serial.println(v);
-    DmxSimple.write(1, v);
-    DmxSimple.write(2, v);
-    DmxSimple.write(4, v);
-    DmxSimple.write(5, v);
-    DmxSimple.write(6, v);
-    DmxSimple.write(7, v);
+  if (stringComplete)
+  {
+    stringComplete = false;
+    getCommand();
+
+    if (commandString.equals("LIGH"))
+    {
+      c = inputString.substring(5, 8);
+      v = inputString.substring(8, 11);
+      DmxSimple.write(c.toInt(), v.toInt());
+    }
+    if (commandString.equals("STOP"))
+    {
+      DmxSimple.write(002, 0);
+    }
+    inputString = "";
+  }
+
+}
+
+void getCommand()
+{
+  if (inputString.length() > 0)
+  {
+    commandString = inputString.substring(1, 5);
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
   }
 }
